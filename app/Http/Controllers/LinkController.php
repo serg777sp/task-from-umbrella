973bcc050c70a2f214fb96e87b9e3e6f92de3227
sergey_posunko <sergey.posunko@progforce.com>
2017-04-14 12:28:05 +0300

    added api route for a creating new link

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -27,6 +27,35 @@ class LinkController extends Controller
 	return redirect('/links');
     }
 
+    //method for saving in the database a new link through api
+    public function storeLinkApi(Request $req) {
+	try{
+	    $res = ['errors' => false ];
+	    $validateRes = $this->getValidateResult($req->all(),[
+		'original_url' => 'required|url',
+		'short_url' => 'unique:links,short_url'
+	    ]);
+	    if($validateRes){
+		$res['errors'] = true;
+		$res['messages'] = $validateRes;
+	    } elseif (!Link::checkUrl($req->original_url)){
+		$res['errors'] = true;
+		$res['messages'][] = 'Url not available or answered with error! Please, enter a correct url';
+	    }
+
+	    if($res['errors']) return response()->json($res);
+
+	    $data['original_url'] = $req->original_url;
+	    $data['short_url'] = $req->short_url ? $req->short_url : Link::generateShortUrl();
+	    Link::create($data);
+	    $res['success'] = true;
+	    return response()->json($res);
+
+	} catch (Exception $e){
+	    dd($e);
+	}
+    }
+
     //method for checking url
     public function checkLink(Request $req) {
 	try{
@@ -69,8 +98,8 @@ class LinkController extends Controller
     }
 
     //method for getting a validations errors messages array
-    private function getValidateResult($arrayData) {
-	$validator = Validator::make($arrayData, $this->rules());
+    private function getValidateResult($arrayData, $rules = []) {
+	$validator = Validator::make($arrayData, empty($rules) ? $this->rules() : $rules);
         if ($validator->fails()) {
 	    return $validator->errors()->messages();
         }
